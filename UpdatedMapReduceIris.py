54a27f57bd456d89feee1ce7698bf6b13f14733a
Ruvimbo Chiweshe <s00220020@atu.ie>
2026-02-14 18:49:34 +0000

    UpdatedMapReduceIris.py

---
+++ UpdatedMapReduceIris.py
@@ -0,0 +1,29 @@
+from mrjob.job import MRJob
+from mrjob.step import MRStep
+import re
+
+DATA_RE = re.compile(r"[\w.-]+")
+
+class MRProb2_3(MRJob):
+
+    def steps(self):
+        return [
+            MRStep(mapper=self.mapper_get_sepW_virginica,
+                   reducer=self.reducer_get_avg)
+        ]
+
+    def mapper_get_sepW_virginica(self, _, line):
+        data = DATA_RE.findall(line)
+        if "Iris-virginica" in data:
+            sep_W = float(data[1])   # still sepal width
+            yield ("sepal width", sep_W)
+
+    def reducer_get_avg(self, key, values):
+        size, total = 0, 0
+        for val in values:
+            size += 1
+            total += val
+        yield ("virginica sepal width avg", round(total, 1) / size)
+
+if __name__ == '__main__':
+    MRProb2_3.run()
